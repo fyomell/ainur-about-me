cdefc7f75a10aeb275db4b9de159037c991cb332
Rofik <rofik7244@gmail.com>
2026-01-13 16:38:46 +0700

    simplify page (pages router)

--- lib/profile.ts
+++ lib/profile.ts
@@ -2,12 +2,38 @@ export const profile = {
   name: "AINUR ROFIK",
   class: "9",
   age: "16",
-  lovers: "FYONA",
+
   supporters: ["GEMINI", "CHATGPT", "PARENTS", "ALLAH"],
+
   favoriteSubjects: ["ASTRONOMY", "INFORMATICS", "SOCIAL STUDIES"],
+
+  goals: ["PRO GAMER", "TRILLIONAIRE"],
+
+  projects: [
+    "TELEGRAM BOT • SSM BOOSTING",
+    "TELEGRAM BOT • CHECK WHATSAPP BIO",
+    "TELEGRAM BOT • SHARE SERVICE",
+    "TELEGRAM BOT • ENCRYPT CODE",
+  ],
+
+  achievements: {
+    courses: [
+      "MICROSOFT WORD COURSE GRADUATE • 2022",
+      "MICROSOFT POWERPOINT COURSE GRADUATE • 2022",
+    ],
+    schoolRanks: [
+      "RANK 3 • SEMESTER 2 • GRADE 1",
+      "RANK 2 • SEMESTER 1 • GRADE 2",
+      "RANK 1 • SEMESTER 2 • GRADE 2",
+      "RANK 2 • SEMESTER 1 • GRADE 3",
+      "RANK 1 • SEMESTER 2 • GRADE 3",
+      "RANK 3 • SEMESTER 2 • GRADE 4",
+    ],
+  },
+
   contact: {
     telegram: "@punyakah",
     email: "rrofik7244@gmail.com",
-    website: "COMING SOON",
+    website: "six-eta.vercel.app",
   },
 };
