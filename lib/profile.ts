3921b5276178ac39b0249bb61bbbb0508a225480
Rofik <rofik7244@gmail.com>
2026-01-13 18:06:09 +0700

    fix: vercel build + update profile schema

--- lib/profile.ts
+++ lib/profile.ts
@@ -3,15 +3,14 @@ export const profile = {
   class: "9",
   age: "16",
 
-  supporters: ["GEMINI", "CHATGPT", "PARENTS", "ALLAH"],
-
+  supporters: ["GEMINI", "CHAT GPT", "PARENTS", "ALLAH"],
   favoriteSubjects: ["ASTRONOMY", "INFORMATICS", "SOCIAL STUDIES"],
 
   goals: ["PRO GAMER", "TRILLIONAIRE"],
 
   projects: [
     "TELEGRAM BOT • SSM BOOSTING",
-    "TELEGRAM BOT • CHECK WHATSAPP BIO",
+    "TELEGRAM BOT • WHATSAPP BIO CHECKER",
     "TELEGRAM BOT • SHARE SERVICE",
     "TELEGRAM BOT • ENCRYPT CODE",
   ],
@@ -22,18 +21,18 @@ export const profile = {
       "MICROSOFT POWERPOINT COURSE GRADUATE • 2022",
     ],
     schoolRanks: [
-      "RANK 3 • SEMESTER 2 • GRADE 1",
-      "RANK 2 • SEMESTER 1 • GRADE 2",
-      "RANK 1 • SEMESTER 2 • GRADE 2",
-      "RANK 2 • SEMESTER 1 • GRADE 3",
-      "RANK 1 • SEMESTER 2 • GRADE 3",
-      "RANK 3 • SEMESTER 2 • GRADE 4",
+      "RANK 3 • GRADE 1 • SEMESTER 2",
+      "RANK 2 • GRADE 2 • SEMESTER 1",
+      "RANK 1 • GRADE 2 • SEMESTER 2",
+      "RANK 2 • GRADE 3 • SEMESTER 1",
+      "RANK 1 • GRADE 3 • SEMESTER 2",
+      "RANK 3 • GRADE 4 • SEMESTER 2",
     ],
   },
 
   contact: {
     telegram: "@punyakah",
     email: "rrofik7244@gmail.com",
-    website: "six-eta.vercel.app",
+    website: "about-me-six-eta.vercel.app",
   },
 };
